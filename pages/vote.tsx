d0b626d2ddfb966bd48c1d302399b7cb922b1e71
Simon <44496148+siim0n@users.noreply.github.com>
2024-05-20 23:30:50 +0200

    test

--- pages/vote.tsx
+++ pages/vote.tsx
@@ -33,7 +33,7 @@ export default function VotePage({ vote_data }: { vote_data: Vote | null }) {
                     <Card key={index} className='mt-5'>
                       <Flex flexDirection='col' justifyContent='center' alignItems='center'>
                         <Title>{vote.title}</Title>
-                        <p>{vote.description}</p>
+                        <p dangerouslySetInnerHTML={{ __html: vote.description }}></p>
 
                         <Flex flexDirection='row' justifyContent='between' alignItems='center'>
                           <span>{vote.votes} votes &bull; {percent ? percent : 0}%</span>
